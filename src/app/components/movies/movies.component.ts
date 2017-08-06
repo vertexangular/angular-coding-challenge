b48cf894db626c3b04295da334df19821bcffc17
vertexangular <vertexangular2@gmail.com>
2017-08-06 22:13:09 +0200

    Added styles to movies component. Known bug: the picutes of movies are smaller than text.

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -10,6 +10,10 @@ import {Observable} from 'rxjs';
 export class MoviesComponent implements OnInit {
     imagePath: string = 'http://image.tmdb.org/t/p/w185/';
     movies: Observable<any>;
+
+    types :string[]=['Drama', 'Fantasy'];
+    length :string[]=['170', '82'];
+
     constructor(private util: UtilService) {
     }
 
@@ -18,14 +22,17 @@ export class MoviesComponent implements OnInit {
             .switchMap(data => {
                 this.movies = data.results;
                 return this.movies.map(movie => {
+                    //Fixed type of genre
+                    movie.genre=this.types[movie.id%2]
+                    movie.length=this.length[movie.id%2]
                     console.log(movie.id)
+                    console.log(movie.genre)
+                    console.log(movie.length)
                     this.util.get(`movie/${movie.id}`)
             })
             })
             .subscribe(data => {
                 console.log(data, 'details..?')
             })
-
     }
-
 }
