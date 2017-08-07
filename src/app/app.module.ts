a7c6c54a35688c763200674e46193c9c1de5984a
vertexangular2@gmail.com <filipbarak94@gmail.com>
2017-08-07 12:36:32 +0200

    adds dynamic data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {UtilService} from './services/util.service';
-import { MoviesComponent } from './components/movies/movies.component';
-import { FilterPipe } from './pipes/filter.pipe';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { WatchlistComponent } from './components/watchlist/watchlist.component';
+import {MoviesComponent} from './components/movies/movies.component';
+import {FilterPipe} from './pipes/filter.pipe';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
+import {WatchlistComponent} from './components/watchlist/watchlist.component';
 import {HttpModule} from '@angular/http';
+import {StarRatingModule} from 'angular-star-rating';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MoviesComponent,
-    FilterPipe,
-    SidebarComponent,
-    WatchlistComponent
-  ],
-  imports: [
-    BrowserModule,
-      HttpModule
-  ],
-  providers: [UtilService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MoviesComponent,
+        FilterPipe,
+        SidebarComponent,
+        WatchlistComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        StarRatingModule.forRoot()
+
+    ],
+    providers: [UtilService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -10,26 +10,37 @@ import {Observable} from 'rxjs';
 export class MoviesComponent implements OnInit {
     imagePath: string = 'http://image.tmdb.org/t/p/w185/';
     movies: Observable<any>;
+    movieDetails: any[] = [];
 
-    types :string[]=['Drama', 'Fantasy'];
-    length :string[]=['170', '82'];
 
     constructor(private util: UtilService) {
     }
 
     ngOnInit() {
         this.util.get('movie/top_rated')
-            .switchMap(data => {
+            .do(data => {
                 this.movies = data.results;
-                return this.movies.map(movie => {
-                    //Fixed type of genre
-                    movie.genre=this.types[movie.id%2]
-                    movie.length=this.length[movie.id%2]
-                    console.log(movie.id)
-                    console.log(movie.genre)
-                    console.log(movie.length)
-                    this.util.get(`movie/${movie.id}`)
+                console.log(this.movies, 'movies...')
             })
+            .switchMap(data => {
+                return Observable.forkJoin(this.movies.map(movie => {
+                    console.log(movie.id, 'movie...')
+                    this.util.get(`movie/${movie.id}`)
+                        .subscribe(data => {
+                            console.log(data, 'data???')
+                            this.movieDetails.push([{
+                                "genre": data.genres.map(genre=>genre.name),
+                                "length": data.runtime,
+                                "rating": data.vote_average / 2
+                            }])
+                            console.log(this.movieDetails, 'moviedetilas/')
+                            //this.genres.push(data.genres.map(genre => genre.name))
+                            //this.lenghts.push(data.runtime);
+
+
+                            //console.log(this.lenghts, 'data..')
+                        })
+                }))
             })
             .subscribe(data => {
                 console.log(data, 'details..?')
