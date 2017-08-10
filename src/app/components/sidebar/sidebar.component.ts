d99fd030cc04bffb1ba7729240d595bba3c6d1f8
vertexangular2@gmail.com <filipbarak94@gmail.com>
2017-08-10 14:37:13 +0200

    adds sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -8,11 +8,57 @@ import {MoviesService} from "../../services/movies.service";
     styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+    tabs: any[] = [];
+    private activeTab;
+    isSignedIn: boolean;
 
     constructor(private utilService: UtilService, public moviesService: MoviesService) {
+        this.tabs = [
+            {
+                name: "LATEST",
+                icon: "fa-clock-o",
+                routeTo: "top_rated"
+            },
+            {
+                name: "POPULAR",
+                icon: "fa-star",
+                routeTo: "top_rated"
+            },
+            {
+                name: "FAVORITES",
+                icon: "fa-heart",
+                routeTo: "favorites"
+            },
+            {
+                name: "WATCHLIST",
+                icon: "fa-list-ul",
+                routeTo: "watchlist"
+            },
+            {
+                name: "SEARCH",
+                icon: "fa-search",
+                routeTo: "top_rated"
+            },
+            {
+                name: "GENRES",
+                icon: "fa-th",
+                routeTo: "top_rated"
+            },
+            {
+                name: "SIGN OUT",
+                icon: "fa-power-off",
+                margin: 'logout',
+                color: 'logout-icon',
+
+            }
+
+
+        ]
+
     }
 
     ngOnInit() {
+        this.activeTab = this.tabs[0].name;
     }
 
     isLoggedIn() {
@@ -27,5 +73,9 @@ export class SidebarComponent implements OnInit {
     signOut() {
         this.utilService.clearLocalData();
         this.isLoggedIn();
+    }
+    setActiveTab(tab){
+        this.activeTab = tab.name;
+        console.log(this.activeTab==tab.name, 'tab...')
     }
 }
